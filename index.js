6e24b7dd9607b7ad81252afedd30247351150b9d
Annie Kang <kang.annie@gmail.com>
2017-01-15 08:19:33 -0800

    verify that id submitted is unique and does not already exist in the database. #1

--- index.js
+++ index.js
@@ -3,7 +3,60 @@ const neo4j = require('neo4j-driver').v1;
 const babel = require('babel-polyfill');
 
 const app = express();
-var driver = neo4j.driver('bolt://localhost', neo4j.auth.basic('neo4j', 'annie'));
+var driver = neo4j.driver('bolt://localhost', neo4j.auth.basic('neo4j', 'kangaroo'));
+
+app.use(express.static('src'));
+
+app.get('/data', (req, res) => {
+ console.log('Data retrieved');
+ res.json('Got it!');
+});
+/*
+app.post('/orgchart', (req, res) => {
+ var session = driver.session();
+ session
+   .run("CREATE (ron:Employee {id: 'rp1', first_name:'Ron', last_name:'Perris', role_title:'Instructor, Founder'}) CREATE (annie:Employee {id: 'ak1', first_name:'Annie', last_name:'Kang', role_title:'Student'}) CREATE (annie)-[rel:REPORTS_TO]->(ron) RETURN annie.id, rel.type, ron.role_title")
+   .then(function(result){
+     console.log(result);
+     const results = result.records.forEach(function(record) {
+       console.log(record);
+     });
+     session.close();
+     res.json(result);
+   })
+   .catch(function(error) {
+     console.log(error);
+   });
+});
+*/
+app.post('/newemployee/:id/:first/:last', (req, res) => {
+  console.log(req.params.id);
+
+  var session = driver.session();
+  session
+    .run("MATCH (check:Employee {id: {id}}) RETURN check.id", {id: req.params.id})
+    .then( function(result){
+      if (result.records.length===0) {
+        return session.run("CREATE (new:Employee {id: {id}, first_name: {first}, last_name: {last} }) RETURN new.id AS id, new.first_name AS first_name, new.last_name AS last_name", {id: req.params.id, first: req.params.first, last: req.params.last})
+      }
+      res.json(req.params.id + ' already exists');
+    })
+    .then( function(result) {
+      const results = result.records[0].get('id');
+      console.log(results);
+      console.log(result);
+      console.log(result.records[0]);
+      session.close();
+      res.json(results);
+    })
+
+    .catch(function(error) {
+      console.log(error);
+      res.json(error);
+    });
+
+});
+
 
 
 app.listen(3000, () => console.log('listening at 3000'));
--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-const newEmployee = document.getElementById('new-employee');
+const newEmployee = document.getElementById('employee');
 
 function createEmployee(event) {
   event.preventDefault();
@@ -12,6 +12,22 @@ function createEmployee(event) {
     manager: document.getElementById('employee-mgr'),
   };
 
+  var myHeaders = new Headers();
+
+  var myInit = { method: 'POST',
+                 headers: myHeaders,
+                 mode: 'cors',
+                 cache: 'default' };
+
+   /*const getData = fetch('/data/', myInit);
+   getData
+     .then(result => result.json())
+     .catch(error => console.error(error.data))
+   */
+   const createNew = fetch('/newemployee/' + newEmployee.id.value + '/' + newEmployee.first.value + '/' + newEmployee.last.value, myInit);
+   createNew
+     .then(result => result.json())
+     .catch(error => console.error(error.data))
 }
 
 newEmployee.addEventListener('submit', createEmployee);
