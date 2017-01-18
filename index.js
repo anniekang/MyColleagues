33cdc44582b1cda49330927dc4f857d28486b9ad
Annie Kang <kang.annie@gmail.com>
2017-01-18 12:07:58 -0800

    User can view employee profile. #2

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ app.post('/newemployee/', (req, res) => {
     .run("MATCH (check:Employee {id: {id}}) RETURN check.id", {id: req.body.id})
     .then( result => {
       if (result.records.length===0) {
-        return session.run("CREATE (new:Employee {id: {id}, first_name: {first}, last_name: {last}, photo: {photo}, job_title: {title}, email: {email}, manager: {manager} }) WITH new MATCH (mgr: Employee {id: {manager}}) CREATE UNIQUE (new)-[rel:REPORTS_TO]->(mgr) RETURN new.id AS id, new.first_name AS first, new.last_name AS last, new.photo AS photo, new.job_title AS title, new.email AS email, new.manager_id AS manager_id, type(rel) AS relationship, mgr.first_name AS manager_first_name, mgr.last_name AS manager_last_name", parameters)
+        return session.run("CREATE (new:Employee {id: {id}, first_name: {first}, last_name: {last}, photo: {photo}, job_title: {title}, email: {email}, manager_id: {manager} }) WITH new MATCH (mgr: Employee {id: {manager}}) CREATE UNIQUE (new)-[rel:REPORTS_TO]->(mgr) RETURN new.id AS id, new.first_name AS first, new.last_name AS last, new.photo AS photo, new.job_title AS title, new.email AS email, new.manager_id AS manager_id, type(rel) AS relationship, mgr.first_name AS manager_first_name, mgr.last_name AS manager_last_name", parameters)
       }
       else {
         session.close();
