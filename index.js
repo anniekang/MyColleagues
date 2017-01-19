fca4177a6b8d53f9719300417f1da91d1118616f
Annie Kang <kang.annie@gmail.com>
2017-01-19 10:51:39 -0800

    User can delete employee profile. #8

--- index.js
+++ index.js
@@ -11,39 +11,22 @@ app.use(express.static('src'));
 
 app.use(bodyParser.json());
 
-app.get('/viewemployee/:id', (req, res) => {
-  var session = driver.session();
-  session
-    .run("MATCH (view:Employee {id: {id}})-[:REPORTS_TO]->(mgr:Employee) RETURN view.id AS id, view.first_name AS first, view.last_name AS last, view.photo AS photo, view.job_title AS title, view.job_description AS description, view.email AS email, view.manager_id AS manager_id, mgr.first_name AS manager_first, mgr.last_name AS manager_last", {id: req.params.id})
-    .then( result => {
-      if (result.records.length===0) {
-        session.close();
-        res.status(400).json({error: 'Employee ' + req.params.id + ' does not exist.'});
-      }
-      else {
-        const results = {};
-        result.records[0].forEach( (value, key) => {
-          results[key] = value;
-        })
-        session.close();
-        res.json(results);
-      }
-    })
-
-    .catch( error => {
-      res.json(error);
-    });
-
-});
-
 app.post('/newemployee/', (req, res) => {
   const parameters = req.body;
   var session = driver.session();
   session
-    .run("MATCH (check:Employee {id: {id}}) RETURN check.id", {id: req.body.id})
+    .run(`
+      MATCH (check:Employee {id: {id}}) RETURN check.id`,
+      {id: req.body.id})
     .then( result => {
       if (result.records.length===0) {
-        return session.run("CREATE (new:Employee {id: {id}, first_name: {first}, last_name: {last}, photo: {photo}, job_title: {title}, job_description: '', email: {email}, manager_id: {managerId} }) WITH new MATCH (mgr: Employee {id: {managerId}}) CREATE UNIQUE (new)-[rel:REPORTS_TO]->(mgr) RETURN new.id AS id, new.first_name AS first, new.last_name AS last, new.photo AS photo, new.job_title AS title, new.job_description AS description, new.email AS email, new.manager_id AS manager_id, type(rel) AS relationship, mgr.first_name AS manager_first, mgr.last_name AS manager_last", parameters)
+        return session.run(`
+          CREATE (new:Employee {id: {id}, first_name: {first}, last_name: {last}, photo: {photo}, job_title: {title}, job_description: '', email: {email}, manager_id: {managerId}})
+          WITH new
+          MATCH (mgr: Employee {id: {managerId}})
+          CREATE UNIQUE (new)-[rel:REPORTS_TO]->(mgr)
+          RETURN new.id AS id, new.first_name AS first, new.last_name AS last, new.photo AS photo, new.job_title AS title, new.job_description AS description, new.email AS email, new.manager_id AS manager_id, type(rel) AS relationship, mgr.first_name AS manager_first, mgr.last_name AS manager_last`,
+          parameters)
       }
       else {
         session.close();
@@ -65,11 +48,43 @@ app.post('/newemployee/', (req, res) => {
 
 });
 
+app.get('/viewemployee/:id', (req, res) => {
+  var session = driver.session();
+  session
+    .run(`
+      MATCH (view:Employee {id: {id}})-[:REPORTS_TO]->(mgr:Employee)
+      RETURN view.id AS id, view.first_name AS first, view.last_name AS last, view.photo AS photo, view.job_title AS title, view.job_description AS description, view.email AS email, view.manager_id AS manager_id, mgr.first_name AS manager_first, mgr.last_name AS manager_last`,
+      {id: req.params.id})
+    .then( result => {
+      if (result.records.length===0) {
+        session.close();
+        res.status(400).json({error: 'Employee ' + req.params.id + ' does not exist.'});
+      }
+      else {
+        const results = {};
+        result.records[0].forEach( (value, key) => {
+          results[key] = value;
+        })
+        session.close();
+        res.json(results);
+      }
+    })
+
+    .catch( error => {
+      res.json(error);
+    });
+
+});
+
 app.put('/updateemployee/', (req, res) => {
   const parameters = req.body;
   var session = driver.session();
   session
-    .run("MATCH (update:Employee {id: {id}})-[:REPORTS_TO]->(mgr:Employee {id: {managerId}}) SET update.first_name = {first}, update.last_name = {last}, update.photo = {photo}, update.job_title = {title}, update.job_description = {description}, update.email = {email}, update.manager_id = {managerId} RETURN update.id AS id, update.first_name AS first, update.last_name AS last, update.photo AS photo, update.job_title AS title, update.job_description AS description, update.email AS email, update.manager_id AS manager_id, mgr.first_name AS manager_first, mgr.last_name AS manager_last", parameters)
+    .run(`
+      MATCH (update:Employee {id: {id}})-[:REPORTS_TO]->(mgr:Employee {id: {managerId}})
+      SET update.first_name = {first}, update.last_name = {last}, update.photo = {photo}, update.job_title = {title}, update.job_description = {description}, update.email = {email}, update.manager_id = {managerId}
+      RETURN update.id AS id, update.first_name AS first, update.last_name AS last, update.photo AS photo, update.job_title AS title, update.job_description AS description, update.email AS email, update.manager_id AS manager_id, mgr.first_name AS manager_first, mgr.last_name AS manager_last`,
+      parameters)
     .then( result => {
       const results = {};
       result.records[0].forEach( (value, key) => {
@@ -88,9 +103,13 @@ app.put('/updateemployee/', (req, res) => {
 app.delete('/deleteemployee/:id', (req, res) => {
   var session = driver.session();
   session
-    .run("MATCH (del:Employee {id: {id}}) DETACH DELETE del", {id: req.params.id})
+    .run(`
+      MATCH (del:Employee {id: {id}}) DETACH DELETE del`,
+      {id: req.params.id})
     .then( () => {
-      return session.run("MATCH (emp:Employee {id: {id}}) RETURN emp", {id: req.params.id})
+      return session.run(`
+                      MATCH (emp:Employee {id: {id}}) RETURN emp`,
+                      {id: req.params.id})
     })
       .then ( result => {
         if (result.records.length===0) {
