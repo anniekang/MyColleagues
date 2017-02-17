896fb52d5c137da5d7401f54968fa686341c919e
Annie Kang <kang.annie@gmail.com>
2017-02-16 17:05:41 -0800

    User can access app on HerokuApp. #24

--- server/index.js
+++ server/index.js
@@ -5,7 +5,13 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-var driver = neo4j.driver('bolt://localhost', neo4j.auth.basic('neo4j', 'students'));
+var graphenedbURL = process.env.GRAPHENEDB_BOLT_URL;
+var graphenedbUser = process.env.GRAPHENEDB_BOLT_USER;
+var graphenedbPass = process.env.GRAPHENEDB_BOLT_PASSWORD;
+
+var driver = neo4j.driver(graphenedbURL, neo4j.auth.basic(graphenedbUser, graphenedbPass));
+
+//var driver = neo4j.driver('bolt://localhost', neo4j.auth.basic('neo4j', 'students'));
 
 app.use(express.static(__dirname + '/public'));
 
